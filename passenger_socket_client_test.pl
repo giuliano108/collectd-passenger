86644ffd5d6491d29d0f9199940d433f3a416184
Giuliano <giuliano@108.bz>
2012-05-17 11:51:28 +0100

    Cleanup

--- passenger_socket_client_test.pl
+++ passenger_socket_client_test.pl
@@ -6,10 +6,12 @@ use MiniXML;
 use PassengerStatus;
 use Data::Dumper;
 
-
 my $xml = PassengerStatus::status();
 die PassengerStatus::last_error."\n" unless defined $xml;
+
 my $stats = MiniXML::Parse($xml);
+
 $Data::Dumper::Indent = 1;
 print Dumper($stats);
+
 exit;
